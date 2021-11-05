99cb7ee19f130989249b30bbf49b49351330db43
Radminas Usackas <usackasr@gmail.com>
2021-11-05 10:37:25 +0000

    Created files, barebones design

---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+function Menu() {
+    return (
+        <div id="menu">
+            <div className="menu-item">
+                Score: 0
+            </div>
+            <div className="menu-item big">
+                Memory Game
+            </div>
+            <div className="menu-item">
+                Best Score: 0
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
