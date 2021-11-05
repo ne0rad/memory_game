72e64d715af372e4f4cbcdba3e2fd075c2b76106
Radminas Usackas <usackasr@gmail.com>
2021-11-05 12:17:25 +0000

    Added score count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+import Gameboard from './components/Gameboard';
 import Menu from './components/Menu';
 import './styles/App.css';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  function updateScore(isAdd) {
+    // true/false to add score or reset
+    if(isAdd) {
+      if(score >= bestScore) {
+        setBestScore(score + 1);
+      }
+      setScore(score + 1);
+    } else {
+      setScore(0);
+    }
+  }
+
   return (
     <div className="App">
-      <Menu />
+      <Menu score={score} bestScore={bestScore}/>
+      <Gameboard updateScore={updateScore}/>
     </div>
   );
 }
